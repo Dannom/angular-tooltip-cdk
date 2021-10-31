cb166c615072c64d0d197b569589861a8606a537
Dannom <danomrad@gmail.com>
2021-10-31 17:25:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-s7zevt-cw3hen
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-s7zevt-cw3hen)
---
+++ app/inside-tooltip/inside-tooltip.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inside-tooltip',
+  templateUrl: './inside-tooltip.component.html',
+  styleUrls: ['./inside-tooltip.component.css'],
+})
+export class InsideTooltipComponent implements OnInit {
+  text = 'asdasd';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
